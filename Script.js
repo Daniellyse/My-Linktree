7fcef9be1d0aea30356e8ef796564d3c2b2edd99
Daniellyse <71364964+Daniellyse@users.noreply.github.com>
2026-06-18 23:01:11 -0700

    Commenting for JS

---
+++ Script.js
@@ -0,0 +1,8 @@
+/*JS colormask
+3 stars that represent RGB
+1 star for black&white text colors w(0,0,0)/b(255,255,255)
+OnHoldDownLCLk number up
+OnHoldDownLCLk number down
+Display R: 0, G: 0, B: text: 0 [on Start @Bottom left footer]
+
+Handling variables Button R1,R2,R3,R4, bkgR,nameR */
