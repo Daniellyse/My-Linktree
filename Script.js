40f77194d0597750bd29f1722028974af752c3b4
Daniellyse <71364964+Daniellyse@users.noreply.github.com>
2026-06-22 10:40:21 -0700

    JS Star
    Every click increments counter, need to increment when button is pressed down

--- Script.js
+++ Script.js
@@ -6,3 +6,8 @@ OnHoldDownLCLk number down
 Display R: 0, G: 0, B: text: 0 [on Start @Bottom left footer]
 
 Handling variables Button R1,R2,R3,R4, bkgR,nameR */
+let num = 0;
+function colorShift_R(){
+    num = num + 1;
+    document.getElementById("testClick").innerHTML = "holding for: "+num;
+}
